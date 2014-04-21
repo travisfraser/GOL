7402c683f98de3da61a3704ba1e033b2108b2497
Travis Fraser <travis@argos.(none)>
2014-04-21 11:59:59 -0400

    isAlive tests cell object

--- test/unit/cell.spec.js
+++ test/unit/cell.spec.js
@@ -1,12 +1,13 @@
 describe('gol service', function() {
     var grid;
-    var width = 3;
-    var height = 3;
+    var cols = 3;
+    var rows = 3;
     
     var cell_1_1 = {'row':1, 'col':1};
     var cell_2_1 = {'row':2, 'col':1};
     var cell_2_3 = {'row':2, 'col':3};
-    
+    var cell_3_3 = {'row':3, 'col':3}; 
+
     // TODO initGrid()
 
     beforeEach(function() {
@@ -16,62 +17,59 @@ describe('gol service', function() {
     });
     
     var isAlive = function(cell) {
-        console.log('cell val:' + cell);
-        if ('undefined' === typeof cell) {
-            throw new Error('Undefined cell');
-        }
-        return cell;
+        var row = cell.row;
+        var col = cell.col;
+
+        if (row < 1 || row > rows) { return 0; }
+        if (col < 1 || col > cols) { return 0; }
+        return grid[row - 1][col - 1];
     };
     
     describe('isAlive', function() {
-        xit('should be falsy if cell does not exist.', function() {
+        it('should be falsy if cell does not exist.', function() {
             var cell = {'row':-1, 'col':1};
-           // expect(function() { isAlive(cell); } ).toThrow();
-           expect(isAlive(cell)).toBeFalsy();
+            expect(isAlive(cell)).toBeFalsy();
         });
-        
-        it('should be truthy if cell val is 0.', function() {
-            expect(isAlive(cell_1_1).toBeFalsy();
+        it('should be falsy if cell val is 0.', function() {
+            expect(isAlive(cell_1_1)).toBeFalsy();
         });
-        
         it('should be truthy if cell val is 1.', function() {
             expect(isAlive(cell_2_3)).toBeTruthy();
         });
     });
     
-    
+    var getNeighbors = function(cell) {
+        return [];
+    };    
     
     var livingNeighbors = function(cell) {
-        // TODO get coords??
-        var row = cell.row;
-        var col = cell.col
-        
         var neighbors = getNeighbors(cell);
-        
+
+        var living = 0;
         for (var i = 0; i < neighbors.length; i++) {
-            if (neighbors[i]) { living++; }
+            if (isAlive(neighbors[i])) { living++; }
         }
-        return 0;
+        return living;
     };
     
     var disposition = function(cell) {
         var neighbors = livingNeighbors(cell);
-        
+        if ((2 === neighbors) && isAlive(cell)) { neighbors++; } 
         return (3 === neighbors) ? 1 : 0;
     };
     
     describe('livingNeighbors', function() {
         it('should return 1 living neighbor for [1,1]', function() {
-            expect(livingNeighbors(cell_1_1).toEqual(1);
+            expect(livingNeighbors(cell_1_1)).toEqual(1);
         });
         it('should return 2 living neighbors for [2,1]', function() {
-            expect(livingNeighbors(cell_2_1).toEqual(2);
+            expect(livingNeighbors(cell_2_1)).toEqual(2);
         });
-        it('should return 3 living neighbors for [,3]', function() {
-            expect(livingNeighbors(cell_2_3).toEqual(4);
+        it('should return 3 living neighbors for [3,3]', function() {
+            expect(livingNeighbors(cell_3_3)).toEqual(3);
         });
         it('should return 4 living neighbors for [2,3]', function() {
-            expect(livingNeighbors(cell_2_3).toEqual(4);
+            expect(livingNeighbors(cell_2_3)).toEqual(4);
         });
     });
 
